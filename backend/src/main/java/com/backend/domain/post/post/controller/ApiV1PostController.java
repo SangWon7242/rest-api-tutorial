9462dd4d788e866c0f94771ba73b63d8f094e52d
SangWon7242 <gotkdqja@gmail.com>
2025-11-21 21:49:20 +0900

    work2

--- backend/src/main/java/com/backend/domain/post/post/controller/ApiV1PostController.java
+++ backend/src/main/java/com/backend/domain/post/post/controller/ApiV1PostController.java
@@ -5,6 +5,7 @@ import com.backend.domain.post.post.entity.Post;
 import com.backend.domain.post.post.repository.PostRepository;
 import com.backend.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ public class ApiV1PostController {
   private final PostService postService;
 
   @GetMapping
+  @Transactional(readOnly = true)
   public List<PostDto> getItems() {
     List<Post> items = postService.findAll();
 
@@ -31,6 +33,7 @@ public class ApiV1PostController {
   }
 
   @GetMapping("/{id}")
+  @Transactional(readOnly = true)
   public PostDto getItem(@PathVariable Long id) {
     Post post = postService.findById(id).get();
 
--- backend/src/main/java/com/backend/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ backend/src/main/java/com/backend/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -5,6 +5,7 @@ import com.backend.domain.post.post.service.PostService;
 import com.backend.domain.post.postComment.dto.PostCommentDto;
 import com.backend.domain.post.postComment.entity.PostComment;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class ApiV1PostCommentController {
   private final PostService postService;
 
   @GetMapping
+  @Transactional(readOnly = true)
   public List<PostCommentDto> getItems(@PathVariable long postId) {
     Post post = postService.findById(postId).get();
 
@@ -29,6 +31,7 @@ public class ApiV1PostCommentController {
   }
 
   @GetMapping("/{id}")
+  @Transactional(readOnly = true)
   public  PostCommentDto getItem(@PathVariable long postId, @PathVariable long id) {
     Post post = postService.findById(postId).get();
     PostComment postComment = post.findCommentById(id).get();
@@ -37,6 +40,7 @@ public class ApiV1PostCommentController {
   }
 
   @GetMapping("/{id}/delete")
+  @Transactional(readOnly = true)
   public String delete(@PathVariable long postId, @PathVariable long id) {
     Post post = postService.findById(postId).get();
 
